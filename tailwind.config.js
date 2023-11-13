6d1d974246364ec63a8ff92a44d828fddd1f74c5
gkdey17cse <gkdey17cse@gmail.com>
2023-11-13 10:55:11 +0530

    Updation 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html","Task1_LandingPage.html","Task2_CurrencyConverter.html"],
+  content: ["./src/index.html","Task1_LandingPage.html","Task2_CurrencyConverter.html","./Task3_RegistrationForm.html"],
   theme: {
     extend: {},
   },
